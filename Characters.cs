84216c3a81ddd70a5c0242660a99d2d9ef1be301
David Ehiagwina <S00271237@atu.ie>
2026-02-14 19:09:06 +0000

    Added json Deserialize

---
+++ Characters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODProject
+{
+    internal class Characters
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Class { get; set; }
+
+        public int Level { get; set; }
+
+        public int Strength { get; set; }
+        public int Constitution { get; set; }
+        public int Wisdom { get; set; }
+        public int Intellegence { get; set; }
+        public int Dexterity { get; set; }
+        public int Charisma { get; set; }
+        public Characters() { }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.Json;
+using System.IO;
 
 namespace OODProject
 {
@@ -28,6 +31,37 @@ namespace OODProject
         private void SelectBtn_Click(object sender, RoutedEventArgs e)
         {
             //Test Push
+        }
+
+        List<Characters> characters = new List<Characters>();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Test Character for example
+            Characters Test = new Characters();
+            Test.Name = "Test";
+            Test.Description = "This is a test character";
+            string fileName = "Characters.json";
+
+
+            //Characters Test2 = new Characters();
+            //Test.Name = "David";
+            //Test.Description = "This is a test character";
+
+            //Serialise the character to JSON and save it to a file
+            //string jsonString = JsonSerializer.Serialize(Test);
+            //File.WriteAllText(fileName, jsonString);
+
+            string textFromFile = File.ReadAllText(fileName);
+            List<Characters> deserializedCharacters = JsonSerializer.Deserialize<List<Characters>>(textFromFile);
+            foreach (Characters character in deserializedCharacters)
+            {
+                characters.Add(character);
+            }
+
+            //characters.Add(Test)
+            CharLbx.ItemsSource = characters;
+
         }
     }
 }
